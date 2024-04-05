38f9655a5127fe32d6c08c250e8f6fb158a22324
SebixR <sebarog@gmail.com>
2024-04-05 14:34:41 +0200

    Initial commit

---
+++ src/test/java/com/example/conventions_backend/ConventionsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conventions_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConventionsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
